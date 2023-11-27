8ad2935434d9ac7c115fc26418cc78a6edf929b3
Gaurav-Tilokani <gaurav.180410107121@gmail.com>
2023-11-27 12:28:24 +0530

    added aptitude functions and their tests

---
+++ src/main/java/org/example/AptitudeFunctions.java
@@ -0,0 +1,328 @@
+package org.example;
+
+public class AptitudeFunctions
+{
+    public String percentageChange(double original, double revised)
+    {
+        try {
+            if (original == 0) {
+                throw new Exception();
+            }
+            double res = ((revised - original) / original) * 100;
+            return Double.toString(res);
+        }catch (Exception e)
+        {
+            return "Invalid original value";
+        }
+    }
+
+    public double degreeToPercentageCalculator(double degree)
+    {
+        return (degree*100)/360;
+    }
+
+    public String fractionToPercentageConverter(double a, double b)
+    {
+        try
+        {
+            if(b==0)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                double res = (a/b)*100;
+                return Double.toString(res);
+            }
+        }catch(Exception e)
+        {
+            return "Value of b cannot be zero";
+        }
+    }
+
+    public String getCountOfDays(int[] arr)
+    {
+        int count = 0;
+        double efficiency=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=0)
+            {
+                count++;
+                efficiency = efficiency + 100/arr[i];
+            }
+        }
+        try
+        {
+            if(count==0)
+            {
+                throw new Exception();
+            }
+            double res = (1/efficiency)*100;
+            return Double.toString(res);
+        }catch (Exception e)
+        {
+            return "Invalid input";
+        }
+
+    }
+
+    public String getNthTermAP(double startValue, double commonDifference, int n)
+    {
+        try {
+            if (n <= 0) {
+                throw new Exception();
+            }
+            double res = startValue + (n - 1) * commonDifference;
+            return Double.toString(res);
+        }catch (Exception e)
+        {
+            return "Invalid count input";
+        }
+    }
+
+    public String findFirstNTermsAP(double startValue, double commonDifference, int n)
+    {
+        String res = Double.toString(startValue);
+        try
+        {
+            if(n<=0)
+            {
+                throw new Exception();
+            }
+            double current = startValue;
+            for(int i=0;i<n-1;i++)
+            {
+                current = current + commonDifference;
+                res = res + "," + Double.toString(current);
+            }
+            return res;
+        }catch(Exception e)
+        {
+            return "Invalid count input";
+        }
+    }
+
+    public String findSumOfFirstNTermsAP(double startValue, double commonDifference, int n)
+    {
+
+        try
+        {
+            if(n<=0)
+            {
+                throw new Exception();
+            }
+            double sum = (n/2)*(2*startValue + (n-1)*commonDifference);
+            String res = Double.toString(sum);
+            return res;
+        }catch(Exception e)
+        {
+            return "Invalid count input";
+        }
+    }
+
+
+    public String getNthTermGP(double startValue, double commonDifference, int n)
+    {
+        try {
+            if (n <= 0) {
+                throw new Exception();
+            }
+            double res = startValue*Math.pow(commonDifference, n-1);
+            return Double.toString(res);
+        }catch (Exception e)
+        {
+            return "Invalid count input";
+        }
+    }
+
+    public String findFirstNTermsGP(double startValue, double commonDifference, int n)
+    {
+        String res = Double.toString(startValue);
+        try
+        {
+            if(n<=0)
+            {
+                throw new Exception();
+            }
+            double current = startValue;
+            for(int i=0;i<n-1;i++)
+            {
+                current = current*commonDifference;
+                res = res + "," + Double.toString(current);
+            }
+            return res;
+        }catch(Exception e)
+        {
+            return "Invalid value of n";
+        }
+    }
+
+    public String findSumOfFirstNTermsGP(double startValue, double commonDifference, int n)
+    {
+
+        try
+        {
+            if(n<=0)
+            {
+                throw new Exception();
+            }
+            double sum = startValue*(Math.pow(commonDifference, n)-1)/(commonDifference-1);
+            String res = Double.toString(sum);
+            return res;
+        }catch(Exception e)
+        {
+            return "Invalid count input";
+        }
+    }
+
+    public String simpleInterest(double principal, double rate, double time)
+    {
+        try {
+            if (principal <= 0 || rate<=0 || time<=0) {
+                throw new Exception();
+            }
+            double si = (principal * rate * time)/ 100;
+            return Double.toString(si);
+        }catch (Exception e)
+        {
+            return "Either principal value or rate value or time value is invalid";
+        }
+    }
+
+    public String compoundInterest(double principal, double rate, double time)
+    {
+        String result;
+        try {
+            if (principal == 0 || rate==0 || time==0) {
+                throw new Exception();
+            }
+            double amount = principal * (Math.pow((1 + rate/100), (time)));
+            double interest = amount - principal;
+            return result = "Amount is: " + amount + " Interest is: " + interest;
+        }catch (Exception e)
+        {
+            return "Either principal value or rate value or time value is invalid";
+
+        }
+    }
+
+    public String compareProfitLoss(double costPrice,double sellingPrice)
+    {
+        String res;
+        try {
+            if (costPrice <= 0 || sellingPrice<=0) {
+                throw new Exception();
+            }
+            else if(costPrice-sellingPrice>0) {
+                double loss=costPrice-sellingPrice;
+                double loss_percent=(loss/costPrice)*100;
+                res="loss: " + loss + ", loss percent: " +loss_percent+"%";
+            }
+            else if(costPrice-sellingPrice<0) {
+                double gain=sellingPrice-costPrice;
+                double gain_percent=(gain/costPrice)*100;
+                res="profit: "+ gain +", profit percent: " +gain_percent+"%";
+            }
+            else {
+                res="No profit, No loss";
+            }
+            return res;
+        }
+        catch (Exception e){return "Either costPrice value or sellingPrice value is invalid";}
+    }
+
+
+    public String distanceSpeedAndTime(double speed,double time)
+    {
+        String res;
+        try {
+            if (speed == 0 || time == 0) {
+                throw new Exception();
+            }
+            double distance = speed * time;
+            res= "Distance is "+distance;
+            return res;
+        }
+        catch (Exception e)
+        {
+            return "Invalid input value";
+        }
+    }
+
+    public double squareArea(double side) {
+        double area = side*side;
+        return area;
+    }
+
+    public double rectangleArea(double length, double breadth) {
+        double area = length*breadth;
+        return area;
+    }
+
+    public double circleArea(double radius) {
+        double pi = 3.14;
+        double area = pi*radius*radius;
+        return area;
+    }
+
+    public double triangleArea(double breadth,double height) {
+        double area = 0.5*breadth*height;
+        return area;
+    }
+
+    public double surfaceAreaOfCube(double side) {
+        double area = 6*side*side;
+        return area;
+    }
+    public double volumeOfCube(double side) {
+        double volume = side*side*side;
+        return volume;
+    }
+
+
+    public double surfaceAreaOfCuboid(double length,double breadth,double height) {
+        double area = 2*(length*breadth+breadth*height+height*length);
+        return area;
+    }
+    public double volumeOfCuboid(double length,double breadth,double height) {
+        double volume = length*breadth*height;
+        return volume;
+    }
+
+
+    public double surfaceAreaOfCylinder(double radius,double height) {
+        double pi = 3.14;
+        double area = 2*pi*radius*(radius+height);
+        return area;
+    }
+
+    public double volumeOfCylinder(double radius,double height) {
+        double pi = 3.14;
+        double volume = pi*radius*radius*height;
+        return volume;
+    }
+
+    public double surfaceAreaOfCone(double radius,double length) {
+        double pi = 3.14;
+        double area = pi*radius*(radius+length);
+        return area;
+    }
+    public double volumeOfCone(double radius, double height) {
+        double pi = 3.14;
+        double volume = 1/3*pi*radius*radius*height;
+        return volume;
+    }
+
+
+    public double surfaceAreaOfSphere(double radius) {
+        double pi = 3.14;
+        double area = 4*pi*radius*radius;
+        return area;
+    }
+    public double volumeOfSphere(double radius) {
+        double pi = 3.14;
+        double volume = 4/3*pi*radius*radius*radius;
+        return volume;
+    }
+}
---
+++ src/test/java/TestSavvySolver.java
@@ -0,0 +1,166 @@
+import org.example.AptitudeFunctions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestSavvySolver
+{
+    private AptitudeFunctions aptitudeFunction;
+
+    @BeforeEach
+    void setup()
+    {
+        aptitudeFunction = new AptitudeFunctions();
+    }
+
+    @Test
+    @DisplayName("percentageChange()=>follows path [1,2,3,6,7]")
+    public void testPercentageChange1()
+    {
+        assertEquals("Invalid original value",aptitudeFunction.percentageChange(0, 10));
+    }
+
+    @Test
+    @DisplayName("percentageChange()=>follows path [1,2,4,5,7]")
+    public void testPercentageChange2()
+    {
+        assertEquals("50.0",aptitudeFunction.percentageChange(10, 15));
+    }
+
+    @Test
+    @DisplayName("findFirstNTermsAP()=>follows path [1,2,3,7,8]")
+    public void testFindFirstNTermsAP1()
+    {
+        assertEquals("Invalid count input",aptitudeFunction.findFirstNTermsAP(1, 3, -1));
+    }
+
+    @Test
+    @DisplayName("findFirstNTermsAP()=>follows path [1,2,4,6,8]")
+    public void testFindFirstNTermsAP2()
+    {
+        assertEquals("1.0",aptitudeFunction.findFirstNTermsAP(1, 3, 1));
+    }
+
+    @Test
+    @DisplayName("findFirstNTermsAP()=>follows path [1,2,4,5,4,5,4,6,8]")
+    public void testFindFirstNTermsAP3()
+    {
+        assertEquals("1.0,4.0,7.0",aptitudeFunction.findFirstNTermsAP(1, 3, 3));
+    }
+
+    @Test
+    @DisplayName("simpleInterest()=>follows path [1,3,5,7]")
+    public void testSimpleInterest1()
+    {
+        assertEquals("Either principal value or rate value or time value is invalid",aptitudeFunction.simpleInterest(100, 3, 0));
+    }
+
+    @Test
+    @DisplayName("simpleInterest()=>follows path [1,2,4,6,7]")
+    public void testSimpleInterest2()
+    {
+        assertEquals("20.0",aptitudeFunction.simpleInterest(100, 10, 2));
+    }
+
+    @Test
+    @DisplayName("compareProfitLoss()=>follows path [1,2,6,8]")
+    public void testCompareProfitLoss1()
+    {
+        assertEquals("Either costPrice value or sellingPrice value is invalid",aptitudeFunction.compareProfitLoss(-100, 100));
+    }
+
+    @Test
+    @DisplayName("compareProfitLoss()=>follows path [1,3,7,8]")
+    public void testCompareProfitLoss2()
+    {
+        assertEquals("loss: 20.0, loss percent: 20.0%",aptitudeFunction.compareProfitLoss(100, 80));
+    }
+
+    @Test
+    @DisplayName("compareProfitLoss()=>follows path [1,4,7,8]")
+    public void testCompareProfitLoss3()
+    {
+        assertEquals("profit: 20.0, profit percent: 20.0%",aptitudeFunction.compareProfitLoss(100, 120));
+    }
+
+    @Test
+    @DisplayName("compareProfitLoss()=>follows path [1,5,7,8]")
+    public void testCompareProfitLoss4()
+    {
+        assertEquals("No profit, No loss",aptitudeFunction.compareProfitLoss(100, 100));
+    }
+
+
+
+
+    @Test
+    @DisplayName("fractionToPercentageConverter()=>follows path [1,2,3,6,7]")
+    public void testFractionToPercentageConverter1()
+    {
+        assertEquals("Value of b cannot be zero",aptitudeFunction.fractionToPercentageConverter(10, 0));
+    }
+    @Test
+    @DisplayName("fractionToPercentageConverter()=>follows path [1,2,4,6,7]")
+    public void testFractionToPercentageConverter2()
+    {
+        assertEquals("50.0",aptitudeFunction.fractionToPercentageConverter(20, 40));
+    }
+
+
+
+    @Test
+    @DisplayName("compoundInterest()=>follows path [1,2,3,5,7]")
+    public void testCompoundInterest1()
+    {
+        assertEquals("Either principal value or rate value or time value is invalid",aptitudeFunction.compoundInterest(0,10,20));
+    }
+    @Test
+    @DisplayName("compoundInterest()=>follows path [1,2,4,6,7]")
+    public void testCompoundInterest2()
+    {
+        assertEquals("Amount is: 1610.5100000000004 Interest is: 610.5100000000004",aptitudeFunction.compoundInterest(1000,10,5));
+    }
+
+
+    @Test
+    @DisplayName("findFirstNTermsGP()=>follows path [1,2,3,7,8]")
+    public void testNthTermOfGP1()
+    {
+        assertEquals("Invalid value of n",aptitudeFunction.findFirstNTermsGP(10,10,0));
+    }
+    @Test
+    @DisplayName("findFirstNTermsGP()=>follows path [1,2,4,5,4,6,8]")
+    public void testNthTermOfGP2()
+    {
+        assertEquals("10.0,100.0,1000.0",aptitudeFunction.findFirstNTermsGP(10,10,3));
+    }
+    @Test
+    @DisplayName("findFirstNTermsGP()=>follows path [1,2,4,6,8]")
+    public void testNthTermOfGP3()
+    {
+        assertEquals("10.0",aptitudeFunction.findFirstNTermsGP(10,10,1));
+    }
+
+    @Test
+    @DisplayName("getCountOfDays()=>follows path [1,2,3,5,2,4,7,9,11]")
+    public void getCountOfDays1()
+    {
+        assertEquals("Invalid input",aptitudeFunction.getCountOfDays(new int[]{}));
+    }
+
+    @Test
+    @DisplayName("getCountOfDays()=>follows path [1,2,3,6,2,4,7,9,11]")
+    public void getCountOfDays2()
+    {
+        assertEquals("1.0",aptitudeFunction.getCountOfDays(new int[]{1}));
+    }
+    @Test
+    @DisplayName("getCountOfDays()=>follows path [1,2,4,7,9,11]")
+    public void getCountOfDays3()
+    {
+        assertEquals("Invalid input",aptitudeFunction.getCountOfDays(new int[]{}));
+    }
+}
